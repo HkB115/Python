401ca351bd92a6c859294302b830898691e1e386
Hunter Bennett <HkB115@users.noreply.github.com>
2016-02-11 19:40:58 -0600

    Update motion.py
    Bug fixes

--- motion.py
+++ motion.py
@@ -21,26 +21,31 @@ except:
  exit(0)
 from PIL import Image
 
-pin_red=0
-pin_green=2
-pin_blue=3
-wiringpi.pinMode(pin_red, wiringpi.GPIO.OUTPUT)
-wiringpi.pinMode(pin_green, wiringpi.GPIO.OUTPUT)
-wiringpi.pinMode(pin_blue, wiringpi.GPIO.OUTPUT)
-pin=wiringpi.digitalWrite
-
 ##### Settings #####
-camera.brightness=50
-camera.ISO=800
-led_array=1
-color_offset=25
-delay=6
-scan=True
-trigger_threshold=2
+delay=5 # Default: 5. Minimum delay between camera triggers in seconds
+detection=True # Enable motion detection to run continuously
+discrepancy_margin=25 # Default: 25. Allow for some differences. Useful if camera is pointed outside
+led_array=True # Enable the LEDBorg module to activate when the camera takes a picture
+led_color='red' # Default: 'red'. Choices: 'red', 'green', or 'blue'
+trigger_threshold=2 # Default: 2. Minimum percent change in images to trigger the camera
 ####################
 
+if(led_array):
+ if(led_color=='red'):
+  led_pin=0
+ elif(led_color=='green'):
+  led_pin=2
+ elif(led_color=='blue'):
+  led_pin=3
+ else:
+  led_pin=0
+ wiringpi.pinMode(led_pin, wiringpi.GPIO.OUTPUT)
+ pin=wiringpi.digitalWrite
+else:
+ led_array=False
+
 images=[]
-while(scan):
+while(detect):
  stream=io.BytesIO()
  with picamera.PiCamera() as comparator:
   comparator.resolution=(64,36)
@@ -51,8 +56,9 @@ while(scan):
   images.append(Image.open(stream))
  else:
   images[0]=Image.open(stream)
- diff=0
+ discrepancies=0
  if(len(images)!=1):
+  images[1]=images[0]
   x=0
   while(x<images[0].size[0]):
    y=0
@@ -61,21 +67,20 @@ while(scan):
     val0=img0[0]+img0[1]+img0[2]
     img1=images[1].getpixel((x,y))
     val1=img1[0]+img1[1]+img1[2]
-    if(abs(val1>val0)>color_offset):
-     diff+=1
+    if(abs(val1-val0)>discrepancy_margin):
+     discrepancies+=1
     y+=1
    x+=1
-  change=(diff*100)/(images[0].size[0]*images[0].size[1])
-  print(str(change)+"% changed.")
-  if(change>trigger_threshold):
+  trigger=(discrepancies*100)/(images[0].size[0]*images[0].size[1])
+  print(str(trigger)+"% changed.")
+  if(trigger>trigger_threshold):
    if(led_array==True):
-    pin(pin_red,1)
+    pin(led_pin,1)
    else:
-    pin(pin_red,0)
+    pin(led_pin,0)
    with picamera.PiCamera() as camera:
     camera.resolution=(1280,720)
     camera.brightness=50
     camera.ISO=800
-    camera.capture(capture00000000,format='jpeg')
-   pin(pin_red,0)
-  images[1]=images[0]
+    camera.capture("capture00000000",format='jpeg')
+   pin(led_pin,0)
