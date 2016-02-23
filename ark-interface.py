f0fc5e53f7e7dfe2639d2bd68cb39efb43433403
Hunter Bennett <HkB115@users.noreply.github.com>
2016-02-23 10:39:38 -0600

    Update ark-interface.py

--- ark-interface.py
+++ ark-interface.py
@@ -18,21 +18,20 @@ def mainmenu():
  print("#                                  #")
  print("####################################")
  choice=input("What would you like to do?")
-
-if(choice==1):
- while(restart=='d'):
-  restart=input("Are you sure you want to restart? y/n")
-  if(restart=='y'):
-   restart()
-  elif(restart=='n'):
-   mainmenu()
-  else
-   restart='d'
-  print("Please enter y or n")
-elif(choice==2):
- update()
-elif(choice==3):
- mainmenu()
-else:
- print("Option not recognized")
- mainmenu()
+ if(choice==1):
+  while(restart=='d'):
+   restart=input("Are you sure you want to restart? y/n")
+   if(restart=='y'):
+    restart()
+   elif(restart=='n'):
+    mainmenu()
+   else
+    restart='d'
+   print("Please enter y or n")
+ elif(choice==2):
+  update()
+ elif(choice==3):
+  mainmenu()
+ else:
+  print("Option not recognized")
+  mainmenu()
