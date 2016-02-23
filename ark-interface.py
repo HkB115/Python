af64c1ebdb9360a97546d7c8eed3343c6def4808
Hunter Bennett <HkB115@users.noreply.github.com>
2016-02-23 02:34:39 -0600

    Update ark-interface.py

--- ark-interface.py
+++ ark-interface.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python
 from sys import argv
+print("1: Restart")
+print("2: Update")
+print("3: Stand there and look pretty")
+choice=input("What would you like to do?")
+
+if choice==1
+do this shit etc
