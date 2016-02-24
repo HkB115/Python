33ffef58842d5f95a7f90acbfb7c04cbff202cf2
Hunter Bennett <HkB115@users.noreply.github.com>
2016-02-23 18:17:56 -0600

    Update ark-interface.py

--- ark-interface.py
+++ ark-interface.py
@@ -1,54 +1,54 @@
 #!/usr/bin/python
 ## Created by HkB115
-ver='02.23.16' # DO NOT CHANGE THIS
+ver = '02.23.16' # DO NOT CHANGE THIS
 from os import name,system
 import socket
 from time import sleep
 
 ######## Configuration ########
-server_address='127.0.0.1' # Enter the IP of the server you are connecting to.
-server_port='' # Enter the port of the server through which you are connecting to. If left empty, defaults to 8888.
+server_address = '127.0.0.1' # Enter the IP of the server you are connecting to.
+server_port = '' # Enter the port of the server through which you are connecting to. If left empty, defaults to 8888.
 
 
 ###############################
 
 def clrdisp():
- system('cls' if name=='nt' else 'clear')
+ system('cls' if name == 'nt' else 'clear')
 
 def main():
  menu={}
- menu[0]="####################################"
- menu[1]="Get server status"
- menu[2]="Restart server"
- menu[3]="Start server"
- menu[4]="Stop server"
- menu[5]="Update server"
- menu[6]="Update this script"
- menu[7]="Exit"
- menu[9]="####################################"
+ menu[0] = "####################################"
+ menu[1] = "Get server status"
+ menu[2] = "Restart server"
+ menu[3] = "Start server"
+ menu[4] = "Stop server"
+ menu[5] = "Update server"
+ menu[6] = "Update this script"
+ menu[7] = "Exit"
+ menu[9] = "####################################"
  while(in_loop):
-  in_loop=True
+  in_loop = True
   clrdisp()
   for entry in menu:
-   if(entry==0 or entry==9):
+   if(entry == 0 or entry == 9):
     print(str(menu[entry]))
    else:
     print("#",str(entry),menu[entry])
-  selection=input("Select an option: ")
+  selection = input("Select an option: ")
   clrdisp()
-  if(selection=='1'):
+  if(selection == '1'):
    server_status()
-  elif(selection=='2'):
+  elif(selection == '2'):
    restart_server()
-  elif(selection=='3'):
+  elif(selection == '3'):
    start_server()
-  elif(selection=='4'):
+  elif(selection == '4'):
    stop_server()
-  elif(selection=='5'):
+  elif(selection == '5'):
    update_server()
-  elif(selection=='6'):
+  elif(selection == '6'):
    update_script()
-  elif(selection=='7'):
+  elif(selection == '7'):
    break
   else:
    print("Option not recognized!")
@@ -59,23 +59,23 @@ def server_status():
  socket.create_connection(server_address,30)
 
 def restart_server():
- in_loop=true
- yn0=input("Are you sure you want to restart the server? (y/n): ")
+ in_loop = true
+ yn0 = input("Are you sure you want to restart the server? (y/n): ")
  clrdisp()
- if(yn0=='y'):
-  yn1=input("Would you like to enable a 10 minute countdown? (y/n): ")
+ if(yn0 == 'y'):
+  yn1 = input("Would you like to enable a 10 minute countdown? (y/n): ")
   while(in_loop):
-   in_loop=True
-   if(yn1=='y'):
+   in_loop = True
+   if(yn1 == 'y'):
     #Send restart command with 10 minute countdown option through socket
-    in_loop=False
-   elif(yn1=='n'):
+    in_loop = False
+   elif(yn1 == 'n'):
     #Send restart command without 10 minute countdown option through socket
-    in_loop=False
+    in_loop = False
    else:
     print("Please enter y or n")
   return()
- elif(yn0=='n'):
+ elif(yn0 == 'n'):
   return()
  else:
   print("Please enter y or n")
@@ -87,12 +87,12 @@ def start_server():
  print("Code here")
 
 def stop_server():
- yn=input("Are you sure you want to restart the server? (y/n): ")
+ yn = input("Are you sure you want to restart the server? (y/n): ")
  clrdisp()
- if(yn=='y'):
+ if(yn == 'y'):
   #Send stop command through socket
   return()
- elif(yn=='n'):
+ elif(yn == 'n'):
   return()
  else:
   print("Please enter y or n")
@@ -104,8 +104,8 @@ def update_server():
 def update_script():
  print("Code here")
 
-client_socket=socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-if(server_port==''):
- server_port=8888
+client_socket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+if(server_port == ''):
+ server_port = 8888
 yn='null'
 main()
