1fce39b9f758ffdd557a13dd58322520a344970f
Hunter Bennett <HkB115@users.noreply.github.com>
2016-02-24 01:59:29 -0600

    Updated version and added comments

--- ark-interface-client.py
+++ ark-interface-client.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python
 ## Created by HkB115
-ver = '16.2.23.00' # DO NOT CHANGE THIS
+ver = '16.02.24.00' # DO NOT CHANGE THIS
 from os import name,system
 import socket
 from sys import exit,version_info
@@ -9,10 +9,9 @@ from time import sleep
 ######## Configuration ########
 server_ip = 'localhost' # Enter the IP of the server you are connecting to. If left empty, defaults to localhost.
 server_port = 8888 # Enter the port of the server through which you are connecting to. If left empty, defaults to 8888.
-
 ###############################
 
-try:
+try: # Sets server_ip to 127.0.0.1 if it is not defined, or if 'localhost' is used.
  if(server_ip == '' or server_ip == 'localhost'):
   server_ip = '127.0.0.1'
  server_ip = str(server_ip)
@@ -20,8 +19,7 @@ except:
  print("Enter a valid IP or domain as a string for the server IP!")
  sleep(3)
  exit()
- 
-try:
+try: # Sets server_port to 8888 if it is not defined.
  if(server_port == None):
   server_port = 8888
  server_port = int(server_port)
@@ -30,11 +28,11 @@ except:
  sleep(3)
  exit()
 
-def clrdisp():
- system('cls' if name == 'nt' else 'clear')
+def clrdisp(): # Defines the function clrdisp.
+ system('cls' if name == 'nt' else 'clear') # Clears the display using 'cls' if the system is Windows or 'clear' if the system is UNIX.
 
-def main():
- menu={}
+def main(): # Defines the function main.
+ menu = {} # Sets up menu as an array.
  menu[0] = "####################################"
  menu[1] = "Get server status"
  menu[2] = "Restart server"
@@ -51,11 +49,13 @@ def main():
     print(str(menu[entry]))
    else:
     print("#",str(entry),menu[entry])
-  selection = userInput("Select an option: ")
+  selection = userInput("Select an option: ") # Stores the user's input into the literal 'selection' as a string.
   clrdisp()
-  if(selection == '1'): # Get server status
+  # Get server status
+  if(selection == '1'):
    server_status()
-  elif(selection == '2'): # Restart server
+  # Restart server
+  elif(selection == '2'):
    inloop = True
    while(inloop):
     clrdisp()
@@ -77,9 +77,11 @@ def main():
      inloop = False
     else:
      print("Please enter y or n")
-  elif(selection == '3'): # Start server
+  # Start server
+  elif(selection == '3'):
    start_server()
-  elif(selection == '4'): # Stop server
+  # Stop server
+  elif(selection == '4'):
    inloop = True
    while(inloop):
     clrdisp()
@@ -91,22 +93,26 @@ def main():
      inloop = False
     else:
      print("Please enter y or n")
+  # Update server
   elif(selection == '5'):
    update_server()
+  # Update this script
   elif(selection == '6'):
    update_script()
+  # Exit the program
   elif(selection == '7'):
    sock.close()
    exit()
+  # Repeats the menu function if an invalid option is given.
   else:
    print("Option not recognized!")
    sleep(3)
-   
-def userInput(msg):
+
+def userInput(msg): 
  if(python3):
-  return input(msg)
+  return input(msg) # Uses the input function if a Python 3 environment is being used.
  else:
-  return raw_input(msg)
+  return raw_input(msg) # Uses the raw_input function if a Python environment less than 3 is being used.
 
 def server_status():
  print("Code here")
@@ -120,45 +126,45 @@ def update_server():
 def update_script():
  print("Code here")
 
-python3 = version_info[0] > 2 # Check for Python3
+python3 = version_info[0] > 2 # Checks for Python 3 environment.
 try:
  print("Connecting to %s through port %s" %server_address)
- server_address = (str(server_ip),int(server_port))
- sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+ server_address = (str(server_ip),int(server_port)) # Stores the specified server IP and port to the literal server_address.
+ sock = socket.socket(socket.AF_INET,socket.SOCK_STREAM) # Attempts to create a socket.
 except:
  print("Failed to create a connection with the server")
- sleep(3)
- exit()
-connected = False
-tries = 0
+ sleep(3) # Sleeps for 3 seconds.
+ exit() # Exits the program.
+connected = False # Stores False to the literal 'connected'.
+tries = 0 # Stores 0 to the literal 'tries'.
 while(connected != True):
  try:
-  sock.create_connection((server_ip,server_port))
-  connected = True
- except:
-  tries += 1
-  sleep(3)
- if(tries > 5):
+  sock.create_connection((server_ip,server_port)) # Attempts to create a connection with the specified server and port.
+  connected = True # Stores True to the literal 'connected'.
+ except: # Increases the number of tries by 1 for each failed attempt at creating a connection.
+  tries += 1 # Adds 1 to the literal 'tries'.
+  sleep(3) # Sleeps for 3 seconds.
+ if(tries > 5): # Exits the program if the number of attempts to create a connection with the server exceeds 5.
   print("Connection timed out")
-  sleep(3)
-  exit()
-try:
+  sleep(3) # Sleeps for 3 seconds.
+  exit() # Exits the program.
+try: # Tries to verify the connection by sending a key to the server. If the key matches, it sends back a string to ensure a secure connection.
  print("Verifying connection...")
- send = "Ping!"
- sock.sendall(send)
- amount_received = 0
- amount_expected = len(send)
- while(amount_received < amount_expected):
+ send = "Ping!" # Message to send the server for verification.
+ sock.sendall(send) # Sends the string 'send' to the connected server.
+ data_received = 0 # Sets up the variable for how much data has been received from the server.
+ data_expected = len(send) # Sets up the variable for how much data is expected to be received from the server.
+ while(data_received < data_expected):
   data = sock.recv(64)
   amount_received += len(data)
-  print('received "%s"' % data)
+  print('received "%s"' % data) # Prints out the data that was received from the server.
 except:
  print("Connection could not be verified")
- sleep(3)
- exit()
+ sleep(3) # Sleeps for 3 seconds.
+ exit() # Exits the program.
 print("Connected!")
-sleep(2)
+sleep(2) # Sleeps for 2 seconds.
 try:
- main()
-finally:
- exit()
+ main() # Calls the function main.
+finally: # Executes the following no matter what.
+ exit() # Exits the program.
