6eda7eb1dd5320732f54d3a47a680d4f01353545
Hunter Bennett <HkB115@users.noreply.github.com>
2016-02-26 01:24:19 -0600

    Added better URL changing method.

--- keyscraper.py
+++ keyscraper.py
@@ -2,6 +2,7 @@
 #Made by HkB115. Feel free to use, modify, or distribute.
 from random import randint, sample
 import re
+#from string import replace
 from sys import exit, version_info
 from time import sleep
 try:
@@ -11,7 +12,7 @@ except:
 
 ######## Configuration ########
 ask_repeat = False # Ask to try again after each successful find. Setting to default makes it repeat automatically.
-base_url = 'http://www.google.com/' # Base url for the site.
+base_url = 'http://www.google.com/*' # URL of the site you will be scraping. Use a * to denote where the URL will change.
 # The character set you wish to use. You may use one of the options or give your own.
 # 'alphanumeric.lower' includes all lowercase letters and all digits.
 # 'alphanumeric.upper' includes all uppercase letters and all digits.
@@ -24,7 +25,7 @@ char_set = 'alphanumeric.all' # Use one of the strings provided above or make yo
 delay = 2 # Delay between retries in seconds. Helps prevent IP blocking.
 max_length = 8 # Maximum length of characters after the base url.
 min_length = 4 # Minimum length of characters after the base url.
-key = '(key)|(test)' # The key you are looking for. You may use regex here.
+key = '(key)|(pass)|(user)' # The key you are looking for. You may use regex here.
 ###############################
 
 python3 = version_info[0] > 2 # Checks for Python 3 environment.
@@ -47,7 +48,7 @@ else:
 if(min_length < 1):
  min_length = 0
 if(max_length < min_length):
- print("[ERROR] Max length must be larger than min length!")
+ print("[FAIL] Max length must be larger than min length!")
  sleep(3)
  exit()
 
@@ -56,10 +57,10 @@ def main():
  while(found != True):
   length = randint(min_length, max_length)
   rand_set = ''.join(sample(char_set, length))
-  url = str(base_url + rand_set)
+  url = base_url.replace('*', rand_set)
   print("\n############ NEW SEARCH ############")
   sleep(delay)
-  print("# [....] Validating %s..." % url)
+  print("# [....] Validating %s" % url)
   try:
    content = urllib.urlopen(url).read()
    print("# [ OK ] Valid URL found. Searching for key...")
@@ -68,14 +69,14 @@ def main():
     print("# [FAIL] Key not found. Starting over...")
    else:
     filename = str(rand_set + '.txt')
-    print("# [ ! ] Key match found! Saving content to %s" % (filename))
+    print("# [ OK ] Key match found! Saving content to %s" % (filename))
     fp = open(filename, 'a')
     fp.write(content)
     fp.close()
     found = True
     print("####################################")
   except:
-   print("# [ERROR] URL could not be reached. Starting over...")
+   print("# [FAIL] URL could not be reached. Starting over...")
    print("####################################")
  return
 
